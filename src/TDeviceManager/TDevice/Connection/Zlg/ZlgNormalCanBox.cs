42ce2a4a4aa6ccb002c40a6a6b5a95518b45736f
勤政 唐 <2362696606@qq.com>
2024-10-23 14:12:41 +0800

    废除原定ZlgCan实现方案

--- src/TDeviceManager/TDevice/Connection/Zlg/ZlgNormalCanBox.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace TDevice.Connection.Zlg;
-
-public class ZlgNormalCanBox(ZlgDeviceType deviceType, int deviceIndex) : ZlgCanBoxBase
-{
-    public override ZlgDeviceType DeviceType { get; } = deviceType;
-    public override int DeviceIndex { get; } = deviceIndex;
-}
